9eb654b4ea9a6ce7c5ac587ec8880e90b5d403c7
INTI0488 <INTI0488@192.168.1.93>
2018-08-23 11:51:29 +0200

    Demba : creation package Dao

---
+++ VoyageNordique/src/main/java/fr/adaming/Dao/ClientDoaImpl.java
@@ -0,0 +1,13 @@
+package fr.adaming.Dao;
+
+import fr.adaming.model.Client;
+
+public class ClientDoaImpl implements IClientDao{
+
+	@Override
+	public Client addClient(Client cl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
