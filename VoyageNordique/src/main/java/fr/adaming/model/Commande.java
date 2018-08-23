12e7747fa7aa2f73443df1daeb98931e939f9817
Adaming <Adaming@192.168.1.164>
2018-08-23 14:40:48 +0200

    Thibault : Model Commande + LigneCommande + les relations entre table (client, commande, lignecommande, offresvoyages)

---
+++ VoyageNordique/src/main/java/fr/adaming/model/Commande.java
@@ -0,0 +1,107 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "commandes")
+public class Commande implements Serializable {
+
+	// declaration des attributs
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idCommande")
+	private int idCommande;
+	private int noCommande;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateCommande;
+
+	// transformation UML en JAVA
+	@ManyToOne
+	@JoinColumn(name = "client_id", referencedColumnName = "idClient")
+	private Client client;
+
+	@OneToMany(mappedBy = "commande")
+	private List<LigneCommande> listeCommandes;
+
+	// declaration des constructeurs
+	public Commande() {
+		super();
+	}
+
+	public Commande(int noCommande, Date dateCommande) {
+		super();
+		this.noCommande = noCommande;
+		this.dateCommande = dateCommande;
+	}
+
+	public Commande(int noCommande, Date dateCommande, Client client) {
+		super();
+		this.noCommande = noCommande;
+		this.dateCommande = dateCommande;
+		this.client = client;
+	}
+
+	public Commande(int idCommande, int noCommande, Date dateCommande, Client client) {
+		super();
+		this.idCommande = idCommande;
+		this.noCommande = noCommande;
+		this.dateCommande = dateCommande;
+		this.client = client;
+	}
+
+	// declaration des getters et setters
+	public int getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(int idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	public int getNoCommande() {
+		return noCommande;
+	}
+
+	public void setNoCommande(int noCommande) {
+		this.noCommande = noCommande;
+	}
+
+	public void setDateCommande(Date dateCommande) {
+		this.dateCommande = dateCommande;
+	}
+
+	public Date getDateCommande() {
+		return dateCommande;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Commande [idCommande=" + idCommande + ", noCommande=" + noCommande + "]";
+	}
+
+}
---
+++ VoyageNordique/src/main/java/fr/adaming/model/LigneCommande.java
@@ -0,0 +1,81 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "lignescommandes")
+public class LigneCommande implements Serializable {
+
+	// declaration des attributs
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idlignecommande")
+	private int idLigneCommande;
+	private int quantite;
+	private double prix;
+
+	// transformation UML en JAVA
+	@ManyToOne
+	@JoinColumn(name = "commande_id", referencedColumnName = "idcommande")
+	private Commande commande;
+
+	@ManyToOne
+	@JoinColumn(name = "offrevoyage_id", referencedColumnName = "idvoyage")
+	private OffreVoyage offreVoyage;
+
+	// declaration des constructeurs
+	public LigneCommande(int idLigneCommande, int quantite, double prix) {
+		super();
+		this.idLigneCommande = idLigneCommande;
+		this.quantite = quantite;
+		this.prix = prix;
+	}
+
+	public LigneCommande(int quantite, double prix) {
+		super();
+		this.quantite = quantite;
+		this.prix = prix;
+	}
+
+	// declaration des getters et setters
+	public int getIdLigneCommande() {
+		return idLigneCommande;
+	}
+
+	public void setIdLigneCommande(int idLigneCommande) {
+		this.idLigneCommande = idLigneCommande;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "LigneCommande [idLigneCommande=" + idLigneCommande + ", quantite=" + quantite + ", prix=" + prix + "]";
+	}
+
+}
